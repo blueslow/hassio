ba2d25e13bdc691f5904f03d9fa77e467c13f7be
Klas Sehlstedt <klas@sehlstedt.se>
2021-05-16 11:54:19 +0200

    Changes made on home assistant

--- python_scripts/twinkly.py
+++ python_scripts/twinkly.py
@@ -29,19 +29,25 @@ LOGIN_DATA = {'challenge': 'AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8='}
 TURN_ON_DATA = {MODE: MODE_ON}
 TURN_OFF_DATA = {MODE: MODE_OFF}
 
+
 def formatData(data):
-  return json.dumps(data).encode('utf8')
+    return json.dumps(data).encode('utf8')
+
 
 def processRequest(request):
-  return urllib.request.urlopen(request)
+    return urllib.request.urlopen(request)
+
 
 def processRequestJSON(request):
-  loginResponse = processRequest(request)
-  reader = codecs.getreader("utf-8")
-  return json.load(reader(loginResponse))
+    loginResponse = processRequest(request)
+    reader = codecs.getreader("utf-8")
+    return json.load(reader(loginResponse))
+
 
 # login to api - get challenge response and auth token
-loginRequest = urllib.request.Request(url = LOGIN_URL, headers = HEADERS, data = formatData(LOGIN_DATA))
+loginRequest = urllib.request.Request(url=LOGIN_URL,
+                                      headers=HEADERS,
+                                      data=formatData(LOGIN_DATA))
 loginData = processRequestJSON(loginRequest)
 
 challengeResponse = loginData[CHALLENGE_RESPONSE]
@@ -51,31 +57,41 @@ HEADERS[AUTH_HEADER] = authToken
 verifyData = {CHALLENGE_RESPONSE: challengeResponse}
 
 # verify token by responding with challenge response
-verifyRequest = urllib.request.Request(url = VERIFY_URL, headers = HEADERS, data = formatData(verifyData))
+verifyRequest = urllib.request.Request(url=VERIFY_URL,
+                                       headers=HEADERS,
+                                       data=formatData(verifyData))
 verifyData = processRequestJSON(verifyRequest)
 
+
 def turnOn():
-  onRequest = urllib.request.Request(url = MODE_URL, headers = HEADERS, data = formatData(TURN_ON_DATA))
-  processRequest(onRequest)
-  print(1)
+    onRequest = urllib.request.Request(url=MODE_URL,
+                                       headers=HEADERS,
+                                       data=formatData(TURN_ON_DATA))
+    processRequest(onRequest)
+    print(1)
+
 
 def turnOff():
-  offRequest = urllib.request.Request(url = MODE_URL, headers = HEADERS, data = formatData(TURN_OFF_DATA))
-  processRequest(offRequest)
-  print(0)
-
-def getState():
-  modeRequest = urllib.request.Request(url = MODE_URL, headers = HEADERS)
-  modeData = processRequestJSON(modeRequest)
-
-  if modeData[MODE] != MODE_OFF:
-    print(1)
-  else:
+    offRequest = urllib.request.Request(url=MODE_URL,
+                                        headers=HEADERS,
+                                        data=formatData(TURN_OFF_DATA))
+    processRequest(offRequest)
     print(0)
 
+
+def getState():
+    modeRequest = urllib.request.Request(url=MODE_URL, headers=HEADERS)
+    modeData = processRequestJSON(modeRequest)
+
+    if modeData[MODE] != MODE_OFF:
+        print(1)
+    else:
+        print(0)
+
+
 if ARG_ACTION == ARG_ON:
-  turnOn()
+    turnOn()
 elif ARG_ACTION == ARG_OFF:
-  turnOff()
+    turnOff()
 elif ARG_ACTION == ARG_STATE:
-  getState()
+    getState()
